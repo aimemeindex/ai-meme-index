a044df1c8d09f7d622d8304c0566c2decaf6fb40
aimemeindex <aicheckup8@gmail.com>
2025-01-08 03:37:23 +0900

    Create npcAgent.ts

---
+++ agents/npcAgent.ts
@@ -0,0 +1,9 @@
+import { Agent } from "@virtuals-protocol/game";
+
+const npcAgent = new Agent({
+  name: "NPC Bartender",
+  personality: "friendly but reactive",
+  capabilities: ["dynamic responses"],
+});
+
+export default npcAgent;
